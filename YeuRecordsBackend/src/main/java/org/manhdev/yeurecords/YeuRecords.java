68f890a42d5398ce622e7fa1b4465bdcf531ab58
manhz2003 <manhthenguyen2003@gmail.com>
2025-03-18 17:42:01 +0700

    fix rename picture frontend

---
+++ YeuRecordsBackend/src/main/java/org/manhdev/yeurecords/YeuRecords.java
@@ -0,0 +1,11 @@
+package org.manhdev.yeurecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeuRecords {
+    public static void main(String[] args) {
+        SpringApplication.run(YeuRecords.class, args);
+    }
+}
